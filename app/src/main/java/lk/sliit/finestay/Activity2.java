3c1b84c063dfbba64b23298e5b7a66ab645be458
Thisuraakila <thisu.asj@gmail.com>
2019-08-04 18:05:21 +0530

    FEEDBACK renamed as

--- app/src/main/java/lk/sliit/finestay/Activity2.java
+++ app/src/main/java/lk/sliit/finestay/Activity2.java
@@ -9,6 +9,6 @@ public class Activity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_add_feedback);
     }
 }
