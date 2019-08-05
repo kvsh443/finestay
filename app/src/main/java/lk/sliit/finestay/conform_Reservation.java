39e1021dbb82f25cb42b942a96efe52d0db5f9f3
kvsh <>
2019-08-05 23:55:28 +0530

    added Reservation Manager Layout

---
+++ app/src/main/java/lk/sliit/finestay/conform_Reservation.java
@@ -0,0 +1,17 @@
+package lk.sliit.finestay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class conform_Reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conform_reserve);
+
+
+
+    }
+}
