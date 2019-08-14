8350d8b27985339bb9ad2da7f0f2e745c65eb18a
drmohotti <drmohotti@gmail.com>
2019-08-14 20:49:03 +0530

    12_drm

--- app/src/main/java/lk/sliit/finestay/login_page.java
+++ app/src/main/java/lk/sliit/finestay/login_page.java
@@ -6,31 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-public class registration_page extends AppCompatActivity {
+public class login_page extends AppCompatActivity {
 
-    Button b1;
-    ImageButton b2;
-
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration_page);
+        setContentView(R.layout.activity_login_page);
 
-        b1 = findViewById(R.id.register);
-
-        b2 = findViewById(R.id.logo);
+        b1 = findViewById(R.id.login);
+        b2 = findViewById(R.id.registration_btn);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        b1.setOnClickListener((new View.OnClickListener() {
+
+        b2.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(registration_page.this,profile.class);
+                Intent intent = new Intent(login_page.this,registration_page.class);
                 startActivity(intent);
             }
         }));
